7e713ce21fa422386b76de49157d1e9fb8d7a134
RicardoSantos-99 <ricardo2010carvalho@hotmail.com.br>
2021-10-18 22:58:18 -0300

    debuging aplication

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from "express"
-import {createCourse} from "./routes"
 
 const app = express()
+app.use(express.json())
 
-app.get("/", createCourse)
+app.get('/', (req, res) => {
+  return res.json({message: 'Hello'})
+})
+
+app.post('/courses', (req, res) => {
+  const { name } = req.body
+  return res.json({name})
+})
 
 app.listen(3333, ()=> {
   console.log("Server On")
