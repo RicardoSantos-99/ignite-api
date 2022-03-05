e018cd089c64f029fbb86a323cc5eb36169b0e2a
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2022-03-05 19:13:00 -0300

    fix(Global): style guides

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express from "express"
-import { categoriesRoutes } from "./routes/categories.routes"
+import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.use("/categories", categoriesRoutes)
+app.use('/categories', categoriesRoutes);
 
-app.listen(3333, ()=> {
-  console.log("Server On")
-})
+app.listen(3333, () => {
+	console.log('Server On');
+});
